0bb7dec7d3ce17668e12f62fc78079a8a6ac76d3
Henrique Barbosa de Souza Jr <henrique@hbveterinaria.com.br>
2020-05-05 20:47:20 -0300

    Criação da estrutura, instalação das dependências, conexão com o banco PostgreSql

---
+++ src/database/index.js
@@ -0,0 +1,10 @@
+module.exports = { 
+    dialect: 'postgre',
+    host: 'localhost',
+    username: 'postgres',
+    password:'123456',
+    database: 'sqlmode',
+    define:{
+        timestamps: true,
+    }
+}
