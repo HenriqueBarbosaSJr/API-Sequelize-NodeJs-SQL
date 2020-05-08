643ffa8f804e1e0319c48f46e64e5e52315580da
Henrique Barbosa de Souza Jr <henrique@hbveterinaria.com.br>
2020-05-08 20:57:12 -0300

    Construção da rota de relatórios

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const UserController = require ('./controllers/UserController');
 const AddressController = require ('./controllers/AddressController');
 const TechControllers = require('./controllers/TechController');
+const ReportController = require('./controllers/ReportCotroller');
 
 const routes = express.Router();
 
@@ -14,5 +15,7 @@ routes.post('/users/:user_id/addresses', AddressController.store );
 routes.get('/users/:user_id/techs', TechControllers.index );
 routes.post('/users/:user_id/techs', TechControllers.store );
 routes.delete('/users/:user_id/techs', TechControllers.delete );
+
+routes.get('/report', ReportController.show );
 
 module.exports = routes;
