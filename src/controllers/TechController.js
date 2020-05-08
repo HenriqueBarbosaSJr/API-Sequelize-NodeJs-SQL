38f74922569121711e7a310d4272c2c72c0b89e4
Henrique Barbosa de Souza Jr <henrique@hbveterinaria.com.br>
2020-05-08 03:02:28 -0300

    Deletar tecnologias relacionadas ao usuário

--- src/controllers/TechController.js
+++ src/controllers/TechController.js
@@ -26,4 +26,24 @@ module.exports = {
 
         return res.json(tech);
     },
+    async delete (req, res){
+        const { user_id } = req.params;
+        const { name } = req = req.body;
+
+        const user = await User.findByPk(user_id);
+
+        if (!user){
+            return res.status(400).json({ error: 'User not found'});
+        }
+        const  tech  = await Tech.findOne({
+            where: { name }
+          });
+        
+        /* O Sequelize cria métodos como removeTech "onde o remove é criado pelo Sequelize e Tech é o nome da classe"
+           ele cria outros vários também como o addTech.
+        */
+        await user.removeTech('tech')
+        // retorna nada porque o item foi removido
+        return res.json();
+    }
 }; 
