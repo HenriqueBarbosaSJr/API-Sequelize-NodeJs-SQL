5dde946bb838979337a2c2c20624d3e268700945
Henrique Barbosa de Souza Jr <henrique@hbveterinaria.com.br>
2020-05-09 10:27:53 -0300

    Listagem de usuários com operador SQL iLike

--- src/controllers/ReportCotroller.js
+++ src/controllers/ReportCotroller.js
@@ -1,10 +1,27 @@
+// operadores de busca do SQL-Postgres que serão utilizados 
+const { Op } = require('sequelize');
 const User = require('../models/User');
 
 
 module.exports = {
     async show(req, res){
+        // Encontar todos os usuários que possui um e-mail de um domínio específico
+        // Desses usuários quero buscar todos que moram em uma rua específica
+        // Desses usuários quero buscar todos que conhece a tech React
 
-        return res.json( { report: true });
+        const users  = await User.findAll({
+            attributes: [ 'name', 'email'],
+            where: {
+                email: {
+                    /* O operador iLike não faz diferença entre letras maiúsculas
+                       e muniscula
+                    */
+                    [Op.iLike]: '%@gmail.com%'
+                }
+            },
+        });
+
+        return res.json(users);
     }
 
 };
